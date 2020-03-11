8952acdbe327d5020aed812940d1f43d66a1f2cc
JunmouWu <1156153185@qq.com>
2020-03-11 20:16:42 +0800

    task1_version2

--- destroyTree.c
+++ destroyTree.c
@@ -4,20 +4,17 @@
 #include "treeStructure.h"
 #include "destroyTree.h"
 
-void destroyTree( Node *node)
-{
-	if( node->child[0] == NULL )
-	{
+void destroyTree(Node* node){
+	if( node->child[0] == NULL ){
+		free(node);
+		node = NULL;
+	}
+	else{
+		for(int i=0; i<4; i++){
+			destroyTree( node->child[i] );
+		}
 		free( node );
 		node = NULL;
 	}
-	else
-	{
-		for( int i=0; i<4; ++i )
-		{
-			destroyTree( node->child[i] );
-
-		}
-	}
-	return;
+	return ;
 }
